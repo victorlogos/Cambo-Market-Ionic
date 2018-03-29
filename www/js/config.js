982ae1de7c3f394b2589664b239ef19a36b17e0f
Victor <victorlogos1993@gmail.com>
2018-03-29 13:58:05 +0700

    second update

--- www/js/config.js
+++ www/js/config.js
@@ -5,7 +5,7 @@ angular.module("woocommerce-api").constant("CONFIG", {
     // site_url: 'http://localhost:8888/mobile/woocommerce-api/wordpress',
 
     // Max period of time to wait for reply from the server, defined in milliseconds.
-    request_timeout: 8000,
+    request_timeout: 12000,
 
     // The url that follows your main domain, the API version is of interest here, v3 is the latest.
     wc_api_endpoint: '/wc-api/v3',
@@ -16,8 +16,8 @@ angular.module("woocommerce-api").constant("CONFIG", {
 
     // Pair of credentials from your woocommerce installation, please refer to the documentation.
     // Apps today
-    wc_consumer_key: 'ck_e81f51800ffdc9ca1dda31c5c658e7706266a7ac',
-    wc_consumer_secret: 'ccs_0aa7a039e417ad768e241c2dea9b00ce96643ac7',
+    wc_consumer_key: 'ck_18d8a43cbd3274e358c8e6a795e6fb727a34c82b',
+    wc_consumer_secret: 'cs_ee95b8b7fb81771837504ba4f74b1791ded978e9',
     // Localhost
     // wc_consumer_key: 'ck_86a13f2c0ec34a4c99374e120dba436d31f069c3',
     // wc_consumer_secret: 'cs_a0bc69e05eced4a8f464c08849c8f543946448d9',
@@ -29,6 +29,6 @@ angular.module("woocommerce-api").constant("CONFIG", {
     reviews_per_page: 6,
 
     // Frontpage Slideshow Category
-    slideshow_category: 'hoodies'
+    slideshow_category: 'fruits'
 
 });
